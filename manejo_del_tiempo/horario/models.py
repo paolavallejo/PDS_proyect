bb9ad9449dbb7d214d7a9cb3b3b38f0e47a2d954
jacoeafit <jmontesca@eafit.edu.co>
2023-04-17 19:15:56 -0500

    creacion de tablas para persistencia de los datos

--- manejo_del_tiempo/horario/models.py
+++ manejo_del_tiempo/horario/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+#choices de dias de semana:
+DIA_LUNES = 'Lunes'
+DIA_MARTES = 'Martes'
+DIA_MIERCOLES = 'Miércoles'
+DIA_JUEVES = 'Jueves'
+DIA_VIERNES = 'Viernes'
+DIA_SABADO = 'Sábado'
+DIA_DOMINGO = 'Domingo'
+DIA_CHOICES = [
+    (DIA_LUNES, 'Lunes'),
+    (DIA_MARTES, 'Martes'),
+    (DIA_MIERCOLES, 'Miércoles'),
+    (DIA_JUEVES, 'Jueves'),
+    (DIA_VIERNES, 'Viernes'),
+    (DIA_SABADO, 'Sábado'),
+    (DIA_DOMINGO, 'Domingo'),
+    ]
+
+
+
+
+class Horario_sueno(models.Model):
+    usuario_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    horas_semanales = models.TimeField(formats=['%H:%M'])
+    hora_maxima_levantar = models.TimeField(formats=['%H:%M'])
+    hora_minima_acostar = models.TimeField(formats=['%H:%M'])
+    longitud_maxima_sueno = models.TimeField(formats=['%H:%M'])
+    longitud_minima_sueno = models.TimeField(formats=['%H:%M'])
+
+
+
+
+class Actividad_no_fija(models.Model):
+    usuario_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    nombre_actividad = models.CharField(max_length=60)
+    descripcion_actividad = models.CharField(max_length=400)
+
+
+
+
+#Todas las actividades no fijas y horarios de sueno luego pasarán a ser actividades fijas.
+class Actividad_fija(models.Model):
+    usuario_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    nombre_actividad = models.CharField(max_length=60)
+    descripcion_actividad = models.CharField(max_length=400)
+    dia_realizacion = models.CharField(max_length=10,choices=DIA_CHOICES)
+    hora_realizacion = models.TimeField(formats=['%H:%M'])
+    demora_actividad = models.TimeField(formats=['%H:%M'])
+    tiempo_transporte = models.TimeField(formats=['%H:%M'])
+    tipo_actividad = models.CharField(max_length=40,choices=[('no_fija','no_fija'),('fija','fija'),('sueno','sueno')])
+
+
+
+
+
+
+
+
+
