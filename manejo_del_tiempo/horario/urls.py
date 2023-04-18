b40399eacf773b2c57782edc68081eb5a9a50b19
jacoeafit <jmontesca@eafit.edu.co>
2023-04-17 22:03:04 -0500

    creación de las vistas principales del proyecto

--- manejo_del_tiempo/horario/urls.py
+++ manejo_del_tiempo/horario/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
 		path('',views.horario, name = 'horario'),
+        path('/user_login',views.user_login, name = 'user_login'),
+        path('/user_logout',views.user_logout, name = 'user_logout'),
+        path('/user_register',views.user_register, name = 'user_register'),
+        path('/suenio',views.suenio, name = 'suenio'),
+        path('/actividades_fijas',views.actividades_fijas, name = 'actividades_fijas'),
+        path('/actividades_fijas/crear_actividades_fijas',views.crear_actividades_fijas, name = 'crear_actividades_fijas'),
+        path('/actividades_fijas/eliminar_actividades_fijas',views.eliminar_actividades_fijas, name = 'eliminar_actividades_fijas'),
+        path('/actividades_no_fijas',views.actividades_no_fijas, name = 'actividades_no_fijas'),
+        path('/actividades_no_fijas/crear_actividades_no_fijas',views.crear_actividades_no_fijas, name = 'crear_actividades_no_fijas'),
+        path('/actividades_no_fijas/eliminar_actividades_no_fijas',views.eliminar_actividades_no_fijas, name = 'eliminar_actividades_no_fijas'),
 ]
--- manejo_del_tiempo/horario/views.py
+++ manejo_del_tiempo/horario/views.py
@@ -1,71 +1,72 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 #Ruta principal:
 def horario(request):
-    pass
+    return HttpResponse("Estás en la página principal")
 
 
 #Registro de usuario, login y logout:
 def user_login(request):
-    pass
+    return HttpResponse("Estás en el user login")
 
 
 
 
 def user_logout(request):
-    pass
+    return HttpResponse("Estás en el user logout")
 
 
 
 
 def user_register(request):
-    pass
+    return HttpResponse("Estás en el user register")
 
 
 
 #Adquirir datos de sueño:
 def suenio(request):
-    pass
+    return HttpResponse("Estás en la pagina de sueño")
 
 
 
 #Adquirir y mostrar actividades fijas:
 def actividades_fijas(request):
-    pass
+    return HttpResponse("Estás en la pagina de actividades fijas")
 
 
 
 def crear_actividades_fijas(request):
-    pass
+    return HttpResponse("Estás en la pagina de crear actividades fijas")
 
 
 
 def eliminar_actividades_fijas(request):
-    pass
+    return HttpResponse("Estás en la pagina de eliminar actividades fijas")
 
 
 
 #Adquirir y mostrar actividades no fijas
 def actividades_no_fijas(request):
-    pass
+    return HttpResponse("Estás en la pagina de actividades no fijas")
 
 
 
 
 def crear_actividades_no_fijas(request):
-    pass
+    return HttpResponse("Estás en la pagina de crear actividades no fijas")
 
 
 
 def eliminar_actividades_no_fijas(request):
-    pass
+    return HttpResponse("Estás en la pagina de eliminar actividades no fijas")
 
 
 
 
 #Vista que crea el horario final y lo muestra en pantalla:
 def horario_final(request):
-    pass
+    return HttpResponse("Estás en la pagina que despliega el horario final")
 
