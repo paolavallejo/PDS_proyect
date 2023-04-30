0d4f4d7d0d6f9b0c47acc3d928ddd9f0e1e2a10e
jacoeafit <jmontesca@eafit.edu.co>
2023-04-30 13:36:47 -0500

    Creation of templates in horario app

--- manejo_del_tiempo/horario/views.py
+++ manejo_del_tiempo/horario/views.py
@@ -1,27 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib import messages #para mandar mensajes de error
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 
 #Ruta principal:
+#@login_required(login_url = reverse('horario:user_login'))
 def horario(request):
-    return HttpResponse("Estás en la página principal")
+    return render(request,"horario.html")
+
 
 
 #Registro de usuario, login y logout:
 def user_login(request):
-    return HttpResponse("Estás en el user login")
+    
+    if request.method == "POST":
+        username = request.POST.get("usuario").lower()
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request,"¡Usuario no existe!")
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect(reverse('horario'))
+        elif user == None:
+            messages.error(request,"¡Clave erronea!")
+
+    return render(request,"user_login.html")
 
 
-
-
+#@login_required(login_url = reverse('horario:user_login'))
 def user_logout(request):
-    return HttpResponse("Estás en el user logout")
+    logout(request)
+    return redirect(reverse('user_login'))
 
 
 
 
 def user_register(request):
-    return HttpResponse("Estás en el user register")
+    
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect(reverse("horario"))
+        
+    return render(request,'user_register.html',{'UserCreationForm':UserCreationForm})
+        
+    
 
 
 
--- manejo_del_tiempo/manejo_del_tiempo/urls.py
+++ manejo_del_tiempo/manejo_del_tiempo/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('horario',include('horario.urls'))
+    path('horario/',include('horario.urls')),
 ]
