6d7ae037ccd8f81fc2f5125d8d373ab16ed8d326
jacoeafit <jmontesca@eafit.edu.co>
2023-05-15 20:08:20 -0500

    modified helpers.py

--- manejo_del_tiempo/horario/helpers.py
+++ manejo_del_tiempo/horario/helpers.py
@@ -61,3 +61,30 @@ def eliminar_posiciones_actividades(actividades):
             posiciones = Position.objects.filter(event_id = actividad)
             posiciones.delete()
 
+
+
+
+# Insertar Sueño
+def insertar_sueño(schedule, horario_suenio):
+
+    constraints = horario_suenio.constraints
+    duracion_max = round(constraints["time_goal"]/7, 0)
+
+    for day in range(len(schedule)):
+        repite = 0
+        for hour in range(len(schedule[day])):
+            if repite < duracion_max:
+                if schedule[day][hour] == None and hour < constraints["max_levantarse"] and constraints["time_goal_counter"] != 0:
+
+                    schedule[day][hour] = horario_suenio
+                    constraints["time_goal_counter"] = constraints["time_goal_counter"] - 1
+                    repite +=1
+
+                if schedule[day][23-hour] == None and (23-hour) >= constraints["max_acostarse"] and constraints["time_goal_counter"] != 0:
+
+                    schedule[day][23-hour] = horario_suenio
+                    constraints["time_goal_counter"] = constraints["time_goal_counter"] - 1
+                    repite +=1
+
+    return schedule
+
